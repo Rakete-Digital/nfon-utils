f1a3cc5ef334edb7a5153eef1498fdd68887c9a1
mrk-oss <gkmarko86@gmail.com>
2025-10-06 16:15:07 +0300

    Refactor transformAsset Function

--- functions/transformAsset.ts
+++ functions/transformAsset.ts
@@ -1,13 +1,17 @@
-const getAssetsHostDomain = () => {
-    return import.meta.env.HOST_ASSETS_URL || '/web-assets';
-}
+const getAssetsHostDomain = (): string =>
+    import.meta.env.HOST_ASSETS_URL ?? '/web-assets';
 
-const transformAsset = (src: string, transformer?: (p: string) => string): string => {
+const transformAsset = (
+    src: string,
+    transformer?: (p: string) => string
+): string => {
+    if (!src) return src;
+
+    const useNfonHost = import.meta.env.NFON_IMAGE_HOST === 'yes';
     let assetSrc = src;
 
-    if (assetSrc && assetSrc.includes('https://a.storyblok.com')) {
-        const assetsHostDomain = getAssetsHostDomain();
-        assetSrc = assetSrc.replace('https://a.storyblok.com', assetsHostDomain);
+    if (useNfonHost && src.includes('https://a.storyblok.com')) {
+        assetSrc = src.replace('https://a.storyblok.com', getAssetsHostDomain());
     }
 
     return transformer ? transformer(assetSrc) : assetSrc;
